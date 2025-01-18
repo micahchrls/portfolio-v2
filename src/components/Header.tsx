c6b6028ae271ca0170a67fd155853b69a22a40be
Micah Mustaham <mciahmustaham@gmail.com>
2025-01-18 09:52:32 +0800

    Refactor Header component and add SocMedNav
    - Updated import paths in Header component for better structure.
    - Replaced section with div in Header for semantic clarity.
    - Added SocMedNav component to include social media links.
    - Modified ContentNav component to change background color of nav indicators for better visibility.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,15 @@
-import ContentNav from "./main/ContentNav";
+import ContentNav from "@/components/main/ContentNav";
 import Profile from "./main/Profile";
+import SocMedNav from "./main/SocMedNav";
 
 const Header = () => {
   return (
-    <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:overflow-hidden lg:py-4 border-">
-      <section>
+    <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24">
+      <div>
         <Profile />
         <ContentNav />
-      </section>
+      </div>
+      <SocMedNav />
     </header>
   );
 };
--- src/components/main/ContentNav.tsx
+++ src/components/main/ContentNav.tsx
@@ -4,7 +4,7 @@ export default function ContentNav() {
       <ul className="mt-16 w-max">
         <li>
           <a className="group flex items-center py-3" href="#about">
-            <span className="nav-indicator mr-4 h-px w-8  transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+            <span className="nav-indicator mr-4 h-px w-8 bg-white transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
             <span className="nav-text text-xs font-bold uppercase tracking-widest group-hover:text-slate-200 group-focus-visible:text-slate-200">
               About
             </span>
@@ -12,7 +12,7 @@ export default function ContentNav() {
         </li>
         <li>
           <a className="group flex items-center py-3 active" href="#experience">
-            <span className="nav-indicator mr-4 h-px w-8  transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+            <span className="nav-indicator mr-4 h-px w-8 bg-white transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
             <span className="nav-text text-xs font-bold uppercase tracking-widest group-hover:text-slate-200 group-focus-visible:text-slate-200">
               Experience
             </span>
@@ -20,7 +20,7 @@ export default function ContentNav() {
         </li>
         <li>
           <a className="group flex items-center py-3 " href="#projects">
-            <span className="nav-indicator mr-4 h-px w-8  transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+            <span className="nav-indicator mr-4 h-px w-8 bg-white transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
             <span className="nav-text text-xs font-bold uppercase tracking-widest group-hover:text-slate-200 group-focus-visible:text-slate-200">
               Projects
             </span>
